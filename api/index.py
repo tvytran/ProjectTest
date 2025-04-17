e6a8f9890c14a4c019d4de4b47b9e145804b56fe
tvytran <tvytran2@gmail.com>
2025-04-17 17:58:04 -0400

    imported rendering templates

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html")  # not base.html
 
-# This is crucial for Vercel
+# For Vercel compatibility
 index = app.wsgi_app
