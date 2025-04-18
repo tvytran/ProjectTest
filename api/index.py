fda79642980ea1bb3b0a6eb1a3c7ad3c3ffb3633
tvytran <tvytran2@gmail.com>
2025-04-17 23:58:16 -0400

    added a new way to refer to static and other files

--- api/index.py
+++ api/index.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+import os
 
-# Tell Flask where to look for templates and static assets
-app = Flask(__name__, static_folder="../static", template_folder="../templates")
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(
+    __name__,
+    static_folder=os.path.join(base_dir, '..', 'static'),
+    template_folder=os.path.join(base_dir, '..', 'templates')
+)
 
 @app.route('/')
 def home():
